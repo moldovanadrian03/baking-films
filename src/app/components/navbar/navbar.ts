1b421c3a98bb4c8a0af3974e599fec0ed95bef64
Adrian <moldovan.adrian.alexandru03@gmail.com>
2026-04-06 15:28:11 +0300

    Implemented the navbar component and made some minor changes.

---
+++ src/app/components/navbar/navbar.ts
@@ -0,0 +1,43 @@
+import { Component, HostListener, OnInit } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [
+    MatIconModule,
+  ],
+  templateUrl: './navbar.html',
+  styleUrl: './navbar.scss',
+})
+export class Navbar implements OnInit{
+  isSidenavOpened: boolean = false;
+  isMobileView: boolean = false;
+  resizeObserver: ResizeObserver | null = null; // Declare the resizeObserver property
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+    this.updateIsMobileView();
+    this.resizeObserver = new ResizeObserver(() => {
+      this.updateIsMobileView();
+    });
+    this.resizeObserver.observe(document.body);
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onWindowResize(event: Event) {
+    this.updateIsMobileView();
+  }
+
+  private updateIsMobileView() {
+    const screenWidth = window.innerWidth;
+    this.isMobileView = screenWidth <= 768;
+  }
+  toggleSidenav() {
+    this.isSidenavOpened = !this.isSidenavOpened;
+  }
+
+}
+
